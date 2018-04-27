f8a45617a7e85fe5c59d65ff49d39fb79018ca67
Chris West (Faux) <git@goeswhere.com>
2018-04-27 22:36:33 +0100

    require -c

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
+extern crate clap;
+
 use std::collections::HashMap;
 use std::io;
 use std::io::BufRead;
 
 fn main() {
+    let args = clap::App::new("sortuniq")
+        .arg(clap::Arg::with_name("count").short("c"))
+        .arg(clap::Arg::with_name("local"))
+        .get_matches();
+    if args.is_present("count") {
+        flat_count()
+    } else {
+        unimplemented!()
+    }
+}
+
+fn flat_count() -> () {
     let stdin = io::stdin();
     let stdin = stdin.lock();
     let mut count: HashMap<String, u64> = HashMap::new();
