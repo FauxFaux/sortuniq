3cb6d4356b60e4f2e2779d1e9c14cbcba5b1c8b5
Chris West (Faux) <git@goeswhere.com>
2019-01-02 12:56:15 +0000

    thinking about tests (also fix println panics)

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 use std::collections::HashSet;
 use std::io;
 use std::io::BufRead;
+use std::io::Write;
 
 fn main() -> io::Result<()> {
     let args = clap::App::new(clap::crate_name!())
@@ -19,39 +20,41 @@ fn main() -> io::Result<()> {
                 .help("filter out nearby repetitions"),
         )
         .get_matches();
-    if args.is_present("local") {
-        local_uniq()
-    } else if args.is_present("count") {
-        flat_count()
-    } else {
-        stable_uniq()
-    }
-}
 
-fn local_uniq() -> io::Result<()> {
     let stdin = io::stdin();
     let stdin = stdin.lock();
 
+    let stdout = io::stdout();
+    let stdout = stdout.lock();
+
+    if args.is_present("local") {
+        local_uniq(stdin, stdout)
+    } else if args.is_present("count") {
+        flat_count(stdin, stdout)
+    } else {
+        stable_uniq(stdin, stdout)
+    }
+}
+
+fn local_uniq<R: BufRead, W: Write>(from: R, mut to: W) -> io::Result<()> {
     let mut seen = lru::LruCache::new(8);
 
-    for line in stdin.lines() {
+    for line in from.lines() {
         let line = line?;
         if let Some(()) = seen.get(&line) {
             continue;
         }
-        println!("{}", line);
+        writeln!(to, "{}", line)?;
         seen.put(line, ());
     }
 
     Ok(())
 }
 
-fn flat_count() -> io::Result<()> {
-    let stdin = io::stdin();
-    let stdin = stdin.lock();
+fn flat_count<R: BufRead, W: Write>(from: R, mut to: W) -> io::Result<()> {
     let mut count: HashMap<String, u64> = HashMap::with_capacity(10_000);
 
-    for line in stdin.lines() {
+    for line in from.lines() {
         let line = line?;
         *count.entry(line).or_insert(0) += 1;
     }
@@ -59,26 +62,35 @@ fn flat_count() -> io::Result<()> {
     let mut vec: Vec<(String, u64)> = count.into_iter().collect();
     vec.sort_by_key(|&(_, v)| v);
     for (k, v) in vec {
-        println!("{:10} {}", v, k);
+        writeln!(to, "{:10} {}", v, k)?;
     }
 
     Ok(())
 }
 
-fn stable_uniq() -> io::Result<()> {
-    let stdin = io::stdin();
-    let stdin = stdin.lock();
-
+fn stable_uniq<R: BufRead, W: Write>(from: R, mut to: W) -> io::Result<()> {
     let mut seen = HashSet::with_capacity(10_000);
 
-    for line in stdin.lines() {
+    for line in from.lines() {
         let line = line?;
         if seen.contains(&line) {
             continue;
         }
-        println!("{}", line);
+        writeln!(to, "{}", line)?;
         seen.insert(line);
     }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io;
+
+    #[test]
+    fn local() {
+        let mut out = Vec::new();
+        super::local_uniq(io::Cursor::new("foo\nbar\n"), &mut out).unwrap();
+        assert_eq!("foo\nbar\n", String::from_utf8(out).unwrap());
+    }
 }
