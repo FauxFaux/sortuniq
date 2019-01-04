f41dd20795cbb69bf593c9bae60581f077373c65
Chris West (Faux) <git@goeswhere.com>
2019-01-04 22:58:58 +0000

    delete non-count code
    f() { cargo build  && for i in 1 2 3; do time <short.txt cargo run  -- -c | tail -n 1; done }
    
    short: 50e6 words
    musl: 6.28-8.01 seconds, stock: 3.54-3.57

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,9 @@
 use std::collections::HashMap;
-use std::collections::HashSet;
 use std::io;
 use std::io::BufRead;
 use std::io::Write;
 
 fn main() -> io::Result<()> {
-    let args = clap::App::new(clap::crate_name!())
-        .version(clap::crate_version!())
-        .arg(
-            clap::Arg::with_name("size-hint")
-                .long("size-hint")
-                .short("s")
-                .takes_value(true)
-                .validator(|v| {
-                    v.parse::<usize>()
-                        .map(|_| ())
-                        .map_err(|e| format!("invalid number: {}", e))
-                })
-                .help("how much space to pre-allocate"),
-        )
-        .arg(
-            clap::Arg::with_name("count")
-                .long("count")
-                .short("c")
-                .help("prefix lines by the number of occurrences"),
-        )
-        .arg(
-            clap::Arg::with_name("local")
-                .long("local")
-                .conflicts_with("count")
-                .help("filter out nearby repetitions"),
-        )
-        .get_matches();
-
-    let size_hint = args
-        .value_of("size-hint")
-        .and_then(|v| v.parse().ok())
-        .and_then(|v| if 0 != v { Some(v) } else { None });
 
     let stdin = io::stdin();
     let stdin = stdin.lock();
@@ -44,28 +11,7 @@ fn main() -> io::Result<()> {
     let stdout = io::stdout();
     let stdout = stdout.lock();
 
-    if args.is_present("local") {
-        local_uniq(stdin, stdout, size_hint.unwrap_or(32))
-    } else if args.is_present("count") {
-        flat_count(stdin, stdout, size_hint.unwrap_or(10_000))
-    } else {
-        stable_uniq(stdin, stdout, size_hint.unwrap_or(10_000))
-    }
-}
-
-fn local_uniq<R: BufRead, W: Write>(from: R, mut to: W, view_distance: usize) -> io::Result<()> {
-    let mut seen = lru::LruCache::new(view_distance);
-
-    for line in from.lines() {
-        let line = line?;
-        if seen.contains(&line) {
-            continue;
-        }
-        writeln!(to, "{}", line)?;
-        seen.put(line, ());
-    }
-
-    Ok(())
+    flat_count(stdin, stdout, 10_000)
 }
 
 fn flat_count<R: BufRead, W: Write>(from: R, mut to: W, size_hint: usize) -> io::Result<()> {
@@ -83,43 +29,4 @@ fn flat_count<R: BufRead, W: Write>(from: R, mut to: W, size_hint: usize) -> io:
     }
 
     Ok(())
-}
-
-fn stable_uniq<R: BufRead, W: Write>(from: R, mut to: W, size_hint: usize) -> io::Result<()> {
-    let mut seen = HashSet::with_capacity(size_hint);
-
-    for line in from.lines() {
-        let line = line?;
-        if seen.contains(&line) {
-            continue;
-        }
-        writeln!(to, "{}", line)?;
-        seen.insert(line);
-    }
-
-    Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    use std::io;
-
-    fn run_local(input: &[u8], view_distance: usize) -> String {
-        let mut out = Vec::with_capacity(input.len() / 8);
-        let input = io::Cursor::new(input);
-        super::local_uniq(input.clone(), &mut out, view_distance).unwrap();
-        String::from_utf8(out).unwrap()
-    }
-
-    #[test]
-    fn local() {
-        let one = "a\nb\nc\nd\n";
-        let two = format!("{0}{0}", one);
-        for view_distance in 1..=3 {
-            assert_eq!(two, run_local(two.as_bytes(), view_distance));
-        }
-        for view_distance in 4..=9 {
-            assert_eq!(one, run_local(two.as_bytes(), view_distance));
-        }
-    }
 }
